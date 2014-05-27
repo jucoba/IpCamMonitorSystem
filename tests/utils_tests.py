e189a204819121266b04e4faebaa4bb8541d85e5
jucoba <jubel.correa@gmail.com>
2014-05-26 20:22:50 -0500

    More params tests

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -14,13 +14,31 @@ class CamTest(unittest.TestCase):
 		self.assertEqual("0",alarm_status)
 
 	def test_parse_result_get_alarm_alias(self):
-                f = open("tests/resources/get_status.cgi","r")
-                get_status_result =  f.read()
-                f.close()
-                paramUtil = ParamUtils()
-                params = paramUtil.parse(get_status_result)
-                alias = params["alias"]
-                self.assertEqual("camara1",alias)
+		f = open("tests/resources/get_status.cgi","r")
+		get_status_result =  f.read()
+		f.close()
+		paramUtil = ParamUtils()
+		params = paramUtil.parse(get_status_result)
+		alias = params["alias"]
+		self.assertEqual("camara1",alias)
+
+	def test_parse_result_get_alarm_sys_ver(self):
+		f = open("tests/resources/get_status.cgi","r")
+		get_status_result =  f.read()
+		f.close()
+		paramUtil = ParamUtils()
+		params = paramUtil.parse(get_status_result)
+		alias = params["sys_ver"]
+		self.assertEqual("11.35.2.51",alias)
+
+	def test_parse_result_get_alarm_sys_now(self):
+		f = open("tests/resources/get_status.cgi","r")
+		get_status_result =  f.read()
+		f.close()
+		paramUtil = ParamUtils()
+		params = paramUtil.parse(get_status_result)
+		alias = params["now"]
+		self.assertEqual("1398625121",alias)		
 
 
 if __name__ == '__main__':
