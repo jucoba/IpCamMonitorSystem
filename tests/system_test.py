f717ed95cd01dcb8e8bdb5b34155276d6bc200c5
jucoba <jubel.correa@gmail.com>
2014-06-29 17:24:12 -0500

    Another system test

--- tests/system_test.py
+++ tests/system_test.py
@@ -3,7 +3,6 @@ import utilsT
 from camera.camera import *
 from nose.tools import *
 from utils.paramUtils import *
-from mock import Mock
 
 class CameraSystem(unittest.TestCase):
 	paramUtil = ParamUtils()
@@ -12,6 +11,11 @@ class CameraSystem(unittest.TestCase):
 	
 
 	#@unittest.skip("skipping system tests")
+	def system_test_get_cam_alert_not_activated(self):
+		url = "192.168.1.101:8080"	
+		cam = Camera(url)		
+		self.assertFalse(cam.motion_detected())
+
 	def system_test_get_cam_alert_not_activated(self):
 		url = "192.168.1.101:8080"	
 		cam = Camera(url)		
